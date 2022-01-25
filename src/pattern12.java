d4a0deb1bd32dbade52fd2c9f4ec6236d3b68df0
RICKY <grajpootga@gmail.com>
2022-01-25 18:39:20 +0530

    initial

---
+++ src/pattern12.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class pattern12 {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int t=0;
+        for (int i = 1; i <= n; i++,t=0)
+        {
+            for (int j = 1; j <=n-i ; j++) {
+                System.out.print(" ");
+            }
+            while(t!=2*i-1)
+            {
+                System.out.print(i);
+                ++t;
+            }
+            System.out.println();
+        }
+        for (int i = n; i >=1 ; i--) {
+            for (int j = 1; j <=n-i ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j <=2*i-1 ; j++) {
+                System.out.print(i);
+            }
+            for (int j = 0; j < i-1; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
